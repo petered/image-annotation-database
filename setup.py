7656f6725501a6be8b93dc129aeda1826347cedb
peter <peter.ed.oconnor@gmail.com>
2024-03-26 09:51:32 -0700

    simplify setup

--- setup.py
+++ setup.py
@@ -12,7 +12,6 @@ setup(
     packages=find_packages(),
     install_requires=[
         'numpy',
-        'git+https://github.com/petered/artemis.git@develop#egg=artemis-ml',
         'tinydb',
     ],
     classifiers=[
